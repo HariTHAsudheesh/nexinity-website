a7acd7b824300aede1d2e2571ea5dbfe38caf07d
HarithaSudheesh <haritha601974@gmail.com>
2026-05-20 19:37:30 +0530

    Added Why Choose Us page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,12 +1,14 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './components/home/home';
 import { AboutComponent } from './components/about/about';
+import { WhyChooseComponent } from './components/why-choose/why-choose';
 import { ServicesComponent } from './components/services/services';
 import { ContactComponent } from './components/contact/contact';
 
 export const routes: Routes = [
     { path: '', component: HomeComponent },
   { path: 'about', component: AboutComponent },
+  { path: 'why-choose', component: WhyChooseComponent },
   { path: 'services', component: ServicesComponent },
   { path: 'contact', component: ContactComponent }
 ];
